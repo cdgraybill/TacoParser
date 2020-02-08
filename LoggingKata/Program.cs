a8a459b79b518dca10838a9e7d72dedf2b6ca2da
Cameron Graybill <cdgraybill@gmail.com>
2020-02-07 19:53:58 -0600

    added comments to walk through steps

--- LoggingKata/Program.cs
+++ LoggingKata/Program.cs
@@ -14,21 +14,21 @@ namespace LoggingKata
         {
             logger.LogInfo("Log initialized");
 
-            var lines = File.ReadAllLines(csvPath);
+            var lines = File.ReadAllLines(csvPath); //Calling all locations
 
-            var parser = new TacoParser();
+            var parser = new TacoParser(); // Creating TacoParser object
 
-            var locations = lines.Select(parser.Parse).ToArray();
+            var locations = lines.Select(parser.Parse).ToArray(); // Creating array for both locations
 
-            ITrackable location1 = locations[0];
+            ITrackable location1 = locations[0]; // Instantiating variables for final answer 
             ITrackable location2 = locations[0];
 
-            GeoCoordinate CorA = new GeoCoordinate(location1.Location.Latitude, location1.Location.Longitude);
+            GeoCoordinate CorA = new GeoCoordinate(location1.Location.Latitude, location1.Location.Longitude); 
             GeoCoordinate CorB = new GeoCoordinate(location2.Location.Latitude, location2.Location.Longitude);
 
             double distance = CorA.GetDistanceTo(CorB);
 
-            for (int i = 0; i < locations.Length; i++)
+            for (int i = 0; i < locations.Length; i++) // Looping through all locations, finding longest distance
             {
                 for (int x = i + 1; x < locations.Length; x++)
                 {
@@ -45,12 +45,9 @@ namespace LoggingKata
                     }
                 }
             }
-            Console.WriteLine($"Location 1: {location1.Name}");
+            Console.WriteLine($"Location 1: {location1.Name}"); // Printing answer
             Console.WriteLine($"Location 2: {location2.Name}");
             Console.WriteLine(distance);
-
-            // TODO:  Find the two Taco Bells in Alabama that are the furthest from one another.
-            // HINT:  You'll need two nested forloops
         }
     }
 }
